5fa3e73fcc3c8d07cfc4c79e47350a76f58996f4
Alonzo Austin <alonzo6546@outlook.com>
2020-11-25 14:23:03 -0500

    if you somehow are able to get to the creation page, you will not be able to create anything

--- src/components/collection.js
+++ src/components/collection.js
@@ -81,7 +81,7 @@ export default function Collection() {
                         Have no Broilers? Press the   
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-door-closed-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4 1a1 1 0 0 0-1 1v13H1.5a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2a1 1 0 0 0-1-1H4zm2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                        </svg> Icon Sign in/Register. If You are SignedIn press the                 
+                        </svg> Icon to Sign in or Register if you do not have an account. If You are SignedIn press the                 
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                         </svg> 
--- src/create.js
+++ src/create.js
@@ -25,7 +25,8 @@ export default function Create() {
     }
 
     return (
-        <div style={{width: '95%', margin: 'auto'}}>
+      <>
+        {currentUser && <div style={{width: '95%', margin: 'auto'}}>
             <input type="text" className="title" value={title} onChange={event => setTitle(event.target.value)} placeholder="Please name this broiler"/>
             <CodeMirror
                 onBeforeChange={(editor, data, value) => {
@@ -43,6 +44,24 @@ export default function Create() {
                 }}
             />
             <button type="submit" className="addCode" onClick={addBroiler}>Add Broiler</button>
-        </div>
+        </div>}
+
+        {!currentUser && 
+            <div>
+                <br />
+                <br />
+                <br />
+                <h1 id="getStarted" style={{width: '80%'}}>
+                    If You are seeing this screen, then you must not be loggedIn. Press the   
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-door-closed-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M4 1a1 1 0 0 0-1 1v13H1.5a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2a1 1 0 0 0-1-1H4zm2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                    </svg> Icon to Sign in or to Register if you do not have an account. Only then can you create a broiler by pressing the 
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                    </svg> Icon.
+                </h1>
+            </div>
+        }
+      </>
     )
 }
